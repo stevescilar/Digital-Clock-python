6a0bf5e466bc34dcbb83f713ce3048b35022b77a
stevescilar <stevemuambi@gmail.com>
2022-04-03 22:05:23 +0300

    loadup

--- file.py
+++ file.py
@@ -4,13 +4,16 @@ root = Tk()
 root.geometry("500x500")
 root.resizable(0,0)
 root.title('Python Clock')
-Label(root,text = 'YOUTUBE - BUG NINZA', font ='arial 20 bold').pack(side=BOTTOM)def time():
+Label(root,text = 'YOUTUBE - BUG NINZA', font ='arial 20 bold').pack(side=BOTTOM)
+def time():
     string = strftime('%H:%M:%S %p')
     mark.config(text = string)
-    mark.after(1000, time)mark = Label(root, 
+    mark.after(1000, time)
+    mark = Label(root, 
             font = ('calibri', 40, 'bold'),
             pady=150,
-            foreground = 'black')mark.pack(anchor = 'center')
-time()
+            foreground = 'black')
+    mark.pack(anchor = 'center')
+    time()
  
-mainloop()
+    mainloop()
